99789e0c777cc3074364f123ea8b669a9977f12f
Hadi Tedi <haditedi@gmail.com>
2023-04-15 19:35:08 +0100

    working

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,30 +9,28 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
+  let animal = req.body.animal;
 
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      prompt: `AI is a friendly chatbot with good knowledge of london. 
+      AI: How can I help you?
+      human: ${animal}`,
+      temperature: 0.9,
+      max_tokens: 200,
     });
+    console.log("COMPLETION", completion.data.choices[0].text);
+    console.log("ANIMAL", animal);
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,22 +39,28 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
 }
 
 function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+  // const capitalizedAnimal =
+  //   animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  // console.log(capitalizedAnimal);
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return `
+  AI is a chatbot that reluctantly answers questions with sarcastic responses:
+
+  You: How many pounds are in a kilogram?
+  AI: This again? There are 2.2 pounds in a kilogram. Please make a note of this.
+  You: What does HTML stand for?
+  AI: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.
+  You: When did the first airplane fly?
+  AI: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.
+  You: What is the meaning of life?
+  AI: I’m not sure. I’ll ask my friend Google.
+  You: ${animal}`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [result, setResult] = useState([
+    "AI: Hi, I am an AI powered by openAI",
+  ]);
+  const bottomRef = useRef(null);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [result]);
 
   async function onSubmit(event) {
     event.preventDefault();
+    console.log(animalInput);
+    setResult((prev) => {
+      return [...prev, `Human: ${animalInput}`];
+    });
+    console.log("RESULT", result);
+
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
@@ -18,13 +31,17 @@ export default function Home() {
       });
 
       const data = await response.json();
+      console.log(data);
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
-      setResult(data.result);
       setAnimalInput("");
-    } catch(error) {
+      setResult((prev) => [...prev, data.result]);
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
@@ -40,18 +57,24 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>MY AWESOME HOTEL</h3>
+        <div className={styles.result}>
+          {result &&
+            result.map((item) => {
+              return <p key={item}>{item}</p>;
+            })}
+        </div>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter your query"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Submit" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div ref={bottomRef} />
       </main>
     </div>
   );
